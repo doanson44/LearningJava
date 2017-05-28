2077c6e8067c7894d00184773f068797230abfcf
sonthai <sonthai@SONTHAI-PC>
2017-05-28 20:10:24 +0700

    Fixed

--- sonthai.java.thinkinpattern/src/state/KissingPrincess.java
+++ sonthai.java.thinkinpattern/src/state/KissingPrincess.java
@@ -1,26 +1,33 @@
 //: state:KissingPrincess.java
 package state;
-import junit.framework.*;
 
-class Creature {
-  private boolean isFrog = true;
-  public void greet() {
-    if(isFrog)
-      System.out.println("Ribbet!");
-    else
-      System.out.println("Darling!");
-  }
-  public void kiss() { isFrog = false; }
+import junit.framework.TestCase;
+
+class Creature2 {
+	private boolean isFrog = true;
+
+	public void greet() {
+		if (isFrog)
+			System.out.println("Ribbet!");
+		else
+			System.out.println("Darling!");
+	}
+
+	public void kiss() {
+		isFrog = false;
+	}
 }
 
-public class KissingPrincess extends TestCase  {
-  Creature creature = new Creature();
-  public void test() {
-    creature.greet();
-    creature.kiss();
-    creature.greet();
-  }
-  public static void main(String args[]) {
-    junit.textui.TestRunner.run(KissingPrincess.class);
-  }
-} ///:~
+public class KissingPrincess extends TestCase {
+	Creature2 creature = new Creature2();
+
+	public void test() {
+		creature.greet();
+		creature.kiss();
+		creature.greet();
+	}
+
+	public static void main(String args[]) {
+		junit.textui.TestRunner.run(KissingPrincess.class);
+	}
+} /// :~
